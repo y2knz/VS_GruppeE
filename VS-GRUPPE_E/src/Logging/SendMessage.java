7f07563142a6a3fb7e62f767f475e1a73a0c7a9d
Niklas <niklas.zoz@web.de>
2024-04-12 14:40:57 +0200

    push

---
+++ VS-GRUPPE_E/src/Logging/SendMessage.java
@@ -0,0 +1,57 @@
+package Logging;
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+
+
+public class SendMessage {
+	
+	private static final int SYSLOG_PORT = 514; // Syslog standard port
+    private static final int MAX_MESSAGE_LENGTH = 1024; // Max length of syslog message
+
+    public static void sendMessage(String hostname, String appName, String messageText) {
+        try {
+            // Create a socket for syslog standard port
+            DatagramSocket socket = new DatagramSocket();
+
+            // Create the SyslogMessage instance
+            AsciiChars.L255 host = new AsciiChars.L255(hostname);
+            AsciiChars.L048 appNameChars = new AsciiChars.L048(appName);
+            AsciiChars.L128 procId = new AsciiChars.L128("");
+            AsciiChars.L032 msgId = new AsciiChars.L032("ID47");
+            SyslogMessage.Message message = new SyslogMessage.TextMessage(messageText);
+            SyslogMessage syslogMessage = new SyslogMessage(
+                    SyslogMessage.Facility.USER,
+                    SyslogMessage.Severity.INFORMATIONAL,
+                    host,
+                    appNameChars,
+                    procId,
+                    msgId,
+                    null,
+                    message
+            );
+
+            // Convert SyslogMessage to byte array
+            byte[] sendData = syslogMessage.toString().getBytes();
+
+            // Create a DatagramPacket with the data and server address
+            DatagramPacket packet = new DatagramPacket(sendData, sendData.length, InetAddress.getLocalHost(), SYSLOG_PORT);
+
+            // Send the DatagramPacket via the socket
+            socket.send(packet);
+
+            // Close the socket
+            socket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        // Example usage: sending a SyslogMessage
+        sendMessage("localhost", "MyApp", "This is a test message from SyslogMessageSender");
+        System.out.println("Gestartet");
+    }
+
+}
---
+++ VS-GRUPPE_E/src/Logging/SyslogMessage.java
@@ -0,0 +1,159 @@
+package Logging;
+
+import java.io.Serializable;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * RFC 5424 (https://datatracker.ietf.org/doc/html/rfc5424) compliant log
+ * messages as immutable Java objects
+ * 
+ * @author Sandro Leuchter
+ *
+ */
+public class SyslogMessage implements Serializable {
+    private static final long serialVersionUID = -5895573029109990861L;
+    private final Facility fac;
+    private final Severity sev;
+    private final AsciiChars.L255 host;
+    private final AsciiChars.L048 appName;
+    private final AsciiChars.L128 procId;
+    private final AsciiChars.L032 msgId;
+    private final StructuredData data;
+    private final Message message;
+
+    public SyslogMessage(Facility fac, Severity sev, AsciiChars.L255 host,
+            AsciiChars.L048 appName, AsciiChars.L128 procId,
+            AsciiChars.L032 msgId, StructuredData data, Message message) {
+        this.fac = fac;
+        this.sev = sev;
+        this.host = host;
+        this.appName = appName;
+        this.procId = procId;
+        this.msgId = msgId;
+        this.data = data;
+        this.message = message;
+    }
+
+    public Facility fac() {
+        return this.fac;
+    }
+
+
+    public Severity sev() {
+        return sev;
+    }
+
+    public AsciiChars.L255 host() {
+        return host;
+    }
+
+    public AsciiChars.L048 appName() {
+        return appName;
+    }
+
+    public AsciiChars.L128 procId() {
+        return procId;
+    }
+
+    public AsciiChars.L032 msgId() {
+        return msgId;
+    }
+
+    public StructuredData data() {
+        return data;
+    }
+
+    public Message message() {
+        return message;
+    }
+
+    public static int version() {
+        return VERSION;
+    }
+
+
+    public static enum Facility {
+        KERNEL, USER, MAIL_SYSTEM, SYS_DAEMON, SECURITY1, INTERNAL, PRINTER,
+        NEWS, UUCP, CLOCK1, SECURITY2, FTP, NTP, AUDIT, ALERT, CLOCK2, LOCAL0,
+        LOCAL1, LOCAL2, LOCAL3, LOCAL4, LOCAL5, LOCAL6, LOCAL7;
+    }
+
+    public static enum Severity {
+        EMERGENCY, ALERT, CRITICAL, ERROR, WARNING, NOTICE, INFORMATIONAL,
+        DEBUG;
+    }
+
+    public static interface Message {
+        public Object message();
+
+        public int length();
+    }
+
+    public static class BinaryMessage implements Message {
+        private Byte[] message;
+
+        public BinaryMessage(Byte[] message) {
+            this.message = message;
+        }
+
+        @Override
+        public String toString() {
+            return message.toString();
+        }
+
+        @Override
+        public Object message() {
+            return this.message;
+        }
+
+        @Override
+        public int length() {
+            return this.message.length;
+        }
+    }
+
+    public static class TextMessage implements Message {
+        private String message; // UTF8
+
+        public TextMessage(String message) {
+            this.message = message;
+        }
+
+        @Override
+        public String toString() {
+            return "\u00EF\u00BB\u00BF" + message.toString();
+        }
+
+        @Override
+        public Object message() {
+            return this.message;
+        }
+
+        @Override
+        public int length() {
+            return this.message.length();
+        }
+    }
+
+    static final int VERSION = 1; // RFC 5424, Mar 2009
+
+    @Override
+    public String toString() {
+        var prival = String.valueOf(fac().ordinal() * 8 + sev().ordinal());
+        var d = "";
+        if (data() != null) {
+            d = " " + data();
+        }
+        var m = "";
+        if (message() != null && message().message() != null
+                && message().length() > 0) {
+            m = " " + message();
+        }
+        var timestamp = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssXXX")
+                .format(new Date());
+        return "<" + prival + ">" + VERSION + " " + timestamp + " "
+                + host().toString() + " " + appName().toString() + " "
+                + procId().toString() + " " + msgId().toString() + d + m;
+    }
+}
