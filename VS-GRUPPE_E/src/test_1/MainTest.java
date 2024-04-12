f05b765440e24da2651964d02a63245398fbd66e
Niklas <niklas.zoz@web.de>
2024-04-12 14:24:57 +0200

    Ports

--- VS-GRUPPE_E/src/test_1/MainTest.java
+++ VS-GRUPPE_E/src/test_1/MainTest.java
@@ -1,4 +1,9 @@
 package test_1;
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+
 import test_1.SyslogMessage.Facility;
 import test_1.SyslogMessage.Message;
 import test_1.SyslogMessage.Severity;
@@ -6,18 +11,51 @@ import test_1.SyslogMessage.TextMessage;
 
 public class MainTest {
 	
-	public static void main(String[] args) {
-		
-		AsciiChars.L255 host = new AsciiChars.L255("localhost");
-	    AsciiChars.L048 appName = new AsciiChars.L048("MyApp");
-	    AsciiChars.L128 procId = new AsciiChars.L128("12345");
-	    AsciiChars.L032 msgId = new AsciiChars.L032("ID123");
-	    Message message = new TextMessage("This is a test message");
-	    SyslogMessage syslogMessage = new SyslogMessage(Facility.USER, Severity.INFORMATIONAL, host, appName, procId, msgId, null, message);
-	    
-		
-		
-		
-	}
+	private static final int SYSLOG_PORT = 8888; // Syslog standard port
+    private static final int MAX_MESSAGE_LENGTH = 1024; // Max length of syslog message
+
+    public static void sendMessage(String hostname, String appName, String messageText) {
+        try {
+            // Create a socket for syslog standard port
+            DatagramSocket socket = new DatagramSocket();
+
+            // Create the SyslogMessage instance
+            AsciiChars.L255 host = new AsciiChars.L255(hostname);
+            AsciiChars.L048 appNameChars = new AsciiChars.L048(appName);
+            AsciiChars.L128 procId = new AsciiChars.L128("");
+            AsciiChars.L032 msgId = new AsciiChars.L032("ID47");
+            SyslogMessage.Message message = new SyslogMessage.TextMessage(messageText);
+            SyslogMessage syslogMessage = new SyslogMessage(
+                    SyslogMessage.Facility.USER,
+                    SyslogMessage.Severity.INFORMATIONAL,
+                    host,
+                    appNameChars,
+                    procId,
+                    msgId,
+                    null,
+                    message
+            );
+
+            // Convert SyslogMessage to byte array
+            byte[] sendData = syslogMessage.toString().getBytes();
+
+            // Create a DatagramPacket with the data and server address
+            DatagramPacket packet = new DatagramPacket(sendData, sendData.length, InetAddress.getLocalHost(), SYSLOG_PORT);
+
+            // Send the DatagramPacket via the socket
+            socket.send(packet);
+
+            // Close the socket
+            socket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        // Example usage: sending a SyslogMessage
+        sendMessage("localhost", "MyApp", "This is a test message from SyslogMessageSender");
+        System.out.println("Gestartet");
+    }
 
 }
