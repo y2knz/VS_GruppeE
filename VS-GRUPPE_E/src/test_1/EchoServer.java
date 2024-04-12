3b61a15b104a0217f53cc83e3bc8cdb973bca897
Niels Kochert <48764692+vescito@users.noreply.github.com>
2024-04-12 12:53:52 +0200

    progress

--- VS-GRUPPE_E/src/test_1/EchoServer.java
+++ VS-GRUPPE_E/src/test_1/EchoServer.java
@@ -1,5 +1,16 @@
 package test_1;
 
+import test_1.AsciiChars;
+import test_1.StructuredData;
+import test_1.SyslogMessage;
+import test_1.StructuredData.Element;
+import test_1.StructuredData.Param;
+import test_1.SyslogMessage.BinaryMessage;
+import test_1.SyslogMessage.Facility;
+import test_1.SyslogMessage.Severity;
+import test_1.SyslogMessage.TextMessage;
+
+
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
@@ -17,6 +28,25 @@ class EchoServer {
 
       while (true) {
         socket.receive(packetIn);
+        System.out.println(packetIn.getAddress());
+        
+        var m1 = new SyslogMessage(//
+                Facility.SECURITY1, //
+                Severity.CRITICAL, //
+                new AsciiChars.L255("mymachine.example.com"), //
+                new AsciiChars.L048("su"), //
+                new AsciiChars.L128(""), //
+                new AsciiChars.L032("ID47"),//
+                new StructuredData()//
+                        .add(Element.newTimeQuality(true, true))
+                        .add(new Element("exampleSDID@32473")
+                                .add(new Param("iut", "3"))
+                                .add(new Param("eventSource", "Application"))
+                                .add(new Param("eventID", "1011")))
+                        .add(new Element("examplePriority@32473")
+                                .add(new Param("class", "high"))),
+                new TextMessage("'su root' failed for lonvick on /dev/pts/8"));
+        
         System.out.println(
             "Received: " + packetIn.getLength() + " bytes: " + new String(packetIn.getData()));
         packetOut.setData(packetIn.getData());
